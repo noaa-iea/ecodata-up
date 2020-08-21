40ff1ec006d7b856a31ee995ad3d23ffedcf4a51
Ben Best <ben@ecoquants.com>
2020-08-21 01:11:59 -0700

    ∆ permissions, xl upload, validate files & flds

--- app.R
+++ app.R
@@ -11,7 +11,7 @@ shelf(
   # utility
   here,fs,glue,stringr,
   # tidyverse
-  readr,dplyr,tidyr,
+  readr,dplyr,tidyr,purrr,readxl,
   # report
   knitr,rmarkdown,DT,
   # package
@@ -41,11 +41,12 @@ shelf(
   AICcmodavg,
   nlme)
 
+select = dplyr::select
 options(shiny.maxRequestSize = 30*1024^2) # 30MB limit
 
 # variables ----
 
-dir_ecodata_src  <- "/share/github/ecodata"
+dir_ecodata_src  <- "/share/github/ecodata_uploader/ecodata"
 dir_uploader     <- here()
 
 datasets <- list(
@@ -134,9 +135,12 @@ ui <- tagList(
                 accept = c(
                   "text/csv",
                   "text/comma-separated-values,text/plain",
-                  ".csv"))),
+                  ".csv",
+                  ".xls",
+                  ".xlsx"))),
             uiOutput("uiSubmit")),
           mainPanel(
+            plotOutput("plot4size"),
             uiOutput("uiUploadSuggestion"),
             uiOutput("uiSubmitted"),
             uiOutput("figs"),
@@ -145,6 +149,26 @@ ui <- tagList(
 
 server <- function(input, output, session) {
 
+  # output$plot4size ----
+  output$plot4size <- renderImage({
+    # A temp file to save the output. It will be deleted after renderImage
+    # sends it, because deleteFile=TRUE.
+    outfile <- tempfile(fileext='.png')
+    
+    # Generate a png
+    png(outfile, width=400, height=400)
+    #hist(rnorm(50))
+    dev.off()
+    
+    # Return a list
+    list(
+      src   = outfile,
+      alt   = "This is alternate text",
+      style = "display: none;")
+  }, 
+  deleteFile = TRUE,
+  outputArgs = list(style = "display:none"))
+  
   # server variables ----  
   output$g_name  = renderText({ input$g_name })
   output$g_email = renderText({ input$g_email })
@@ -201,8 +225,11 @@ server <- function(input, output, session) {
         git push origin --delete {values$git_branch}
       fi
       
-      # create new local branch and 
+      # create new local branch 
       git checkout -b {values$git_branch}
+      git clean -f -d
+      git reset
+      git checkout .
       git branch --set-upstream-to=origin/{values$git_branch} {values$git_branch}
       git remote set-url origin https://bbest:{gh_pat}@github.com/bbest/ecodata.git
       git pull
@@ -233,7 +260,7 @@ server <- function(input, output, session) {
         # turn off file chmod modifications
         cd {values$dir_ecodata_branch}
         git config core.fileMode false
-        chmod -R 777 ../{values$dir_ecodata_branch}
+        chmod -R 775 ../{values$dir_ecodata_branch}
         
         {git_branch_delete_new_cmds}")
     }
@@ -241,7 +268,7 @@ server <- function(input, output, session) {
     system(git_cmds)
   }
   
-  git_commit_push_request <- function(){
+  git_commit_push_request <- function(comments = ""){
     # install once 
     
     # setup once on server in Terminal:
@@ -253,12 +280,31 @@ server <- function(input, output, session) {
     # sudo su - shiny
     # cd '/share/github/ecodata_bdbest@gmail.com_slopewater'
     # hub pr list -h 'bdbest@gmail.com_slopewater' -f '%U|%i %t'
+    # # enter username, password when prompted
+    # 
+    # # permission woes:
+    # cd /share/github; sudo chmod -R 777 ecodata_uploader;  sudo chown -R shiny ecodata_uploader
+    # cd /share/github; sudo chmod -R 777 iea-uploader/data; sudo chown -R shiny iea-uploader/data
+    #
+    # # restart app: 
+    # touch /share/github/iea-uploader/restart.txt
 
     dataset <- get_dataset()
     
     if (nrow(dataset) == 1 & values$load_success & values$plot_success){
       # branch ecodata src
-      git_msg <- glue("{input$g_email} updating {dataset$dataset_code} dataset via Uploader app")
+      
+      sanitize_comments <- function(txt){
+        txt <- txt %>% 
+          str_replace_all("'", '"') %>% 
+          str_replace_all('{', "[") %>% 
+          str_replace_all('}', "]")
+        txt
+      }
+      
+      comments_sanitized <- sanitize_comments(comments)
+
+      git_msg <- glue("{input$g_email} updating {dataset$dataset_code} dataset via Uploader app\n{comments_sanitized}")
       git_cmds <- glue(
         "
         cd '{values$dir_ecodata_branch}'
@@ -303,6 +349,11 @@ server <- function(input, output, session) {
     }
   }
   
+  rm <- function(path){
+    if (file.exists(path)) file_delete(path)}
+  chmod <- function(path, mode = "775"){
+    if (file.exists(path)) file_chmod(path, mode)}
+  
   # observe dataset selection ----
   observeEvent(input$dt_datasets_rows_selected, {
     values$load_success   <- F
@@ -337,7 +388,35 @@ server <- function(input, output, session) {
     if (is.null(values$upload_state)) {
       return(NULL)
     } else if (values$upload_state == 'uploaded') {
-      return(input$inFile)
+      
+      in_files <- input$inFile %>% 
+        as_tibble() %>% 
+        mutate(
+          is_xl = path_ext(name) %in% c("xls","xlsx"))
+      
+      xl_files <- in_files %>% 
+        filter(is_xl) %>% 
+        mutate(
+          xl    = datapath,
+          type  = "xl",
+          sheet = map(datapath, function(x) excel_sheets(x))) %>% 
+        unnest(sheet) %>% 
+        mutate(
+          name     = glue("{sheet}.csv"),
+          datapath = map2_chr(xl, sheet, function(xl, sheet){
+            tmp_csv <- tempfile(fileext = ".csv")
+            read_excel(xl, sheet) %>% 
+              write_csv(tmp_csv)
+            tmp_csv})) %>% 
+        select(name, size, type, datapath)
+
+      in_files <- in_files %>%
+        filter(!is_xl) %>% 
+        select(-is_xl) %>% 
+        bind_rows(
+          xl_files)
+
+      return(in_files)
     } else if (values$upload_state == 'reset') {
       return(NULL)
     }
@@ -376,6 +455,8 @@ server <- function(input, output, session) {
     bPaginate  = F, 
     info       = F))
   
+  
+  
   # output$uiUploadSuggestion ----
   output$uiUploadSuggestion <- renderUI({
     show_suggestion = !is.null(input$g_id) & length(input$dt_datasets_rows_selected) == 1 && !values$load_success
@@ -389,6 +470,28 @@ server <- function(input, output, session) {
       
       file_url_pfx <- "https://github.com/NOAA-EDAB/ecodata/blob/master/data-raw"
       file_links   <- lapply(files, function(f) a(f, href = glue("{file_url_pfx}/{f}"), target="_blank")) %>% tagList()
+      
+      get_Rfiles <- function(){
+        plot_R <- str_split(dataset$plot_chunks, ", ", simplify=T) %>% as.vector()
+        
+        r_tbl <- tibble(
+          Operation = "Load",
+          R_path    = glue("data-raw/get_{dataset$dataset_code}.R")) %>% 
+          bind_rows(
+            tibble(
+              Operation = "Plot",
+              R_path    = glue("chunk-scripts/{plot_R}"))) %>% 
+          mutate(
+            R_url   = glue("https://github.com/NOAA-EDAB/ecodata/blob/master/{R_path}"),
+            R_fname = basename(R_path),
+            `R script` = glue("<a href='{R_url}' target='_blank'>{R_fname}</a>")) %>% 
+          select(Operation, `R script`)
+        
+        tagList(
+          r_tbl %>% 
+            kable(escape = F, table.attr = "class='kable'") %>% 
+            HTML())
+      }
 
       get_preview <- function(){
         if (!file.exists(file1_csv)) return("")
@@ -399,40 +502,45 @@ server <- function(input, output, session) {
       }
       
       tagList(
-        glue("Please upload the following {length(files)} {ifelse(is_files, 'files:', 'file:')}"), file_links, ".", br(), br(),
-        get_preview())
+        glue("Please upload the following {length(files)} {ifelse(is_files, 'files', 'file')} (or as sheets without the '.csv' in an Excel file):"), file_links, ".", br(), br(),
+        get_preview(),br(),br(),
+        "These files will be processed with the following:",
+        get_Rfiles())
     }
   })
   
-  
-  load_data <- function(){
+  load_data <- function(dataset){
 
     values$load_success <- F
     
-    tryCatch(
-      {
-        dataset       <- get_dataset()
-        
-        message(glue("in load_data(): {dataset$dataset_code}"))
-        
-        dataset_files <- str_split(dataset$dataset_files, ", ", simplify = T) %>% as.vector()
-        
-        file_move(file_input()$datapath, glue("{values$dir_ecodata_branch}/data-raw/{file_input()$name}"))
-        
-        load_R <- glue("{values$dir_ecodata_branch}/data-raw/get_{dataset$dataset_code}.R")
-        
-        system(glue("cd {values$dir_ecodata_branch}; Rscript {load_R}"))
-        
-        load_all(values$dir_ecodata_branch)
-      
-        d <- get(dataset$dataset_code, pos = "package:ecodata")
-        
-        values$load_success <- T
-      },
-      error = function(e) {
-        stop(safeError(e))
-      })
+    dataset_files <- str_split(dataset$dataset_files, ", ", simplify = T) %>% as.vector()
     
+    files_dest <- glue("{values$dir_ecodata_branch}/data-raw/{file_input()$name}")
+    file_move(file_input()$datapath, files_dest)
+    walk(files_dest, chmod, mode = "775")
+    
+    load_R   <- glue("{values$dir_ecodata_branch}/data-raw/get_{dataset$dataset_code}.R")
+    load_log <- glue("{values$dir_ecodata_branch}/data-raw/get_{dataset$dataset_code}_log.txt")
+    
+    res <- system(glue("cd {values$dir_ecodata_branch}; Rscript {load_R} 2>{load_log}"), intern = T)
+    
+    # catch error
+    if (!is.null(attr(res, "status")) && attr(res, "status") == 1){
+      log <- readLines(load_log) %>% paste(collapse = "\n")
+      stop(paste(
+        "loading dataset within ecodata R package:\n\n----\n", 
+        glue("source(data-raw/get_{dataset$dataset_code}.R)"), 
+        "\n----\nHere is a log of messages from running above code:\n----\n", 
+        log, 
+        "\n----\nPlease try uploading fixed file(s)."))
+    }
+    
+    load_all(values$dir_ecodata_branch)
+    
+    d <- get(dataset$dataset_code, pos = "package:ecodata")
+    
+    values$load_success <- T
+
     d
   }
   
@@ -456,109 +564,198 @@ server <- function(input, output, session) {
   # output$figs: executes load_data(), plot chunks, git_commit_push_request() ----
   output$figs <- renderUI({
     req(file_input())
+    #req(session$clientData$output_plot4size_hidden == F)
+    #req(! "output_plot4size_hidden" %in% names(session$clientData))
     
     dataset <- get_dataset()
+    
+    # validate uploaded files ----
     
     dataset_files <- str_split(dataset$dataset_files, ", ", simplify = T) %>% as.vector()
     
     message(glue("in output$figs about to validate"))
+    
+    missing_files <- setdiff(dataset_files, file_input()$name) %>% paste(collapse = ", ")
+    extra_files   <- setdiff(file_input()$name, dataset_files) %>% paste(collapse = ", ")
+    
     validate(
       need(
-        length(dataset_files) == nrow(file_input()), 
-        message = glue(
-          "Expecting {length(dataset_files)} files for this dataset.
-          Uploaded {nrow(file_input())}.
-          Please try again."))
-      # TODO: check filenames match exactly, report on setdiff()
-      # TODO: check header names & value types per file
+        missing_files == "", 
+        message = paste(
+          "Missing files:", missing_files,
+          ifelse(
+            nchar(extra_files) > 0, 
+            paste('\nRename one of these extra files?', extra_files),
+            ""),
+          "\nPlease try uploading again with fixed file names."))
     )
-  
-    # load new data
-    data <- load_data()
+    
+    # validate uploaded fields ----
+    
+    get_flds_type <- function(tbl_paths){
+      tbl_paths %>% 
+        mutate(
+          data = map(path, function(x) read_csv(x)),
+          flds_type = map(data, function(x){
+            flds  <- names(x)
+            types <- sapply(x, class)
+            glue("{flds} <{types}>")
+          })) %>% 
+        select(-data, -path) %>% 
+        unnest(flds_type)
+    }
+    
+    d_files <- tibble(
+      dfile = str_split(dataset$dataset_files, ", ", simplify = T) %>% as.vector()) %>% 
+      mutate(
+        fld_dataset = T,
+        path = glue("{values$dir_ecodata_branch}/data-raw/{dfile}")) %>% 
+      get_flds_type()
+    
+    u_files <- file_input() %>% 
+      as_tibble() %>% 
+      select(dfile = name, path = datapath) %>% 
+      mutate(
+        fld_upload = T) %>% 
+      get_flds_type()
+    u_files
+    
+    du_files <- d_files %>% 
+      full_join(
+        u_files, 
+        by = c("dfile", "flds_type")) %>% 
+      arrange(dfile, fld_dataset, flds_type, fld_upload) %>% 
+      select(dfile, flds_type, fld_dataset, fld_upload) %>% 
+      filter(is.na(fld_dataset) | is.na(fld_upload)) %>% 
+      group_by(dfile) %>% 
+      nest(data = -dfile) %>% 
+      mutate(
+        str_flds_missing = map_chr(data, function(d){
+          d %>% 
+            filter(fld_dataset) %>% 
+            pull(flds_type) %>% 
+            paste(collapse = ", ")}),
+        str_flds_extra = map_chr(data, function(d){
+          d %>% 
+            filter(fld_upload) %>% 
+            pull(flds_type) %>% 
+            paste(collapse = ", ")})) %>% 
+      select(-data) %>% 
+      filter(nchar(str_flds_missing) > 0) %>% 
+      mutate(
+        str_missing = glue("  * {dfile}: {str_flds_missing}"),
+        str_extra   = glue("  * {dfile}: {str_flds_extra}"))
+    
+    validate(
+      need(
+        nrow(du_files) == 0, 
+        message = paste0(
+          "Missing columns <class> in the following files:\n", 
+          paste(du_files$str_missing, collapse = "\n"),
+          "\nRename extra columns <class>?\n",
+          paste(du_files$str_extra, collapse = "\n"),
+          "\nPlease try uploading again with fixed columns.")))
 
-    # tryCatch(
-    #   {
-        
+    # load new data ----
+    data <- load_data(dataset)
+
     message(glue("in output$figs about to plot"))
     
     # figure (https://shiny.rstudio.com/articles/images.html)
     fig_dpi    <- 72
-    fig_width  <- session$clientData$output_fig_width / fig_dpi
-    fig_height <- session$clientData$output_fig_height / fig_dpi
+    #fig_width  <- session$clientData$output_fig_width / fig_dpi
+    #fig_height <- session$clientData$output_fig_height / fig_dpi
+    if (session$clientData$output_plot4size_hidden){
+      fig_width_in  <- 6
+      fig_height_in <- 4
+      message("output_plot4size_hidden: TRUE")
+    } else {
+      fig_width_in  <- session$clientData$output_plot4size_width / fig_dpi %>% round(2)
+      fig_height_in <- session$clientData$output_plot4size_height / fig_dpi %>% round(2)
+      message("output_plot4size_hidden: FALSE")
+    }
     
-    plot_pfx <- here(glue("data/{input$g_email}/{dataset$dataset_code}/{dataset$dataset_code}"))
-    dir_create(dirname(plot_pfx))
+    # plot_www <- glue("{dir_uploader}/www/figures/{input$g_email}_{basename(plot_png)}")
+    # plot_pfx <- here(glue("data/{input$g_email}/{dataset$dataset_code}/{dataset$dataset_code}"))
+    # dir_create(dirname(plot_pfx))
     
     plot_chunks <- str_split(dataset$plot_chunks, ", ", simplify=T) %>% as.vector()
     rmd_grp     <- str_replace(plot_chunks[1], "^(.*)(\\.Rmd.*)$", "\\1")
     
     plot_chunk_to_tab_panel <- function(plot_chunk){
-      if (length(plot_chunks) > 1){
-        plot_rgn <- str_replace(plot_chunk, "^(.*?)(\\.Rmd)-(.*?)-(.*)\\.R$", "\\3")
-        plot_R   <- glue("{plot_pfx}_{plot_rgn}_plot.R")
-        plot_png <- glue("{plot_pfx}_{plot_rgn}.png")
-      } else {
-        plot_rgn <- "plot"
-        plot_R   <- glue("{plot_pfx}_plot.R")
-        plot_png <- glue("{plot_pfx}.png")
-      }
       
-      message(glue("\nplot_R: {plot_R}\n\n"))
+      plot_rgn <- ifelse(
+        length(plot_chunks) > 1,
+        str_replace(plot_chunk, "^(.*?)(\\.Rmd)-(.*?)-(.*)\\.R$", "\\3"),
+        "plot")
       
-      plot_www <- glue("{dir_uploader}/www/figures/{input$g_email}_{basename(plot_png)}")
-      plot_img <- glue("./figures/{input$g_email}_{basename(plot_png)}")
-      dir_create(dirname(plot_www))
+      plot_pfx <- glue(
+        "{dir_uploader}/www/figures/{input$g_email}/{dataset$dataset_code}_{plot_rgn}")
+      plot_R    <- glue("{plot_pfx}.R")
+      plot_log  <- glue("{plot_pfx}_Rlog.txt")
+      plot_png  <- glue("{plot_pfx}.png")
+      plot_img  <- glue("./figures/{input$g_email}/{basename(plot_png)}")
+      dir_create(dirname(plot_pfx), mode = "u=rw,go=rw")
+      message(glue("\nplot_R: {plot_R} -> {plot_png}\n\n"))
       
+      walk(c(plot_R, plot_png, plot_log), rm)
+
       setup_R_files <- tibble(
         file = list.files(glue("{values$dir_ecodata_branch}/chunk-scripts"), glue("{rmd_grp}.*-setup.R")),
         nchar = nchar(file)) %>% 
         arrange(nchar) %>% 
         pull(file)
       setup_R <- paste(glue("source(here('chunk-scripts/{setup_R_files}'))"), collapse = "\n") # cat(setup_R)
-      
       plot_code  <- glue("
           library(here)
           
           {setup_R}
           source(here('chunk-scripts/{plot_chunk}'))
-          
-          ggsave('{plot_png}', width = {fig_width}, height = {fig_height}, dpi = {fig_dpi})
-          ") # cat(plot_code)
-      message(c("\nplot_code:\n", plot_code, "\n\n"))
+        
+          ggsave('{plot_png}', width = {fig_width_in}, height = {fig_height_in}, dpi = {fig_dpi})") # cat(plot_code)
+      message(c(glue("\nplot_code -> {plot_R}:\n"), plot_code, "\n\n"))
       writeLines(plot_code, plot_R)
-      system(glue("cd {values$dir_ecodata_branch}; chmod 777 {plot_R}; Rscript {plot_R}"))
+      file_chmod(plot_R, "775")
+      cmd <- glue("cd {values$dir_ecodata_branch}; Rscript {plot_R} 2>{plot_log}")
+      res <- system(cmd, intern=T)
+      walk(c(plot_png, plot_log), chmod, mode = "775")
       
+      # catch error
+      if (!is.null(attr(res, "status")) && attr(res, "status") == 1){
+        log <- readLines(plot_log) %>% paste(collapse = "\n")
+        
+        stop(paste(
+          "plotting dataset within ecodata R package:\n\n----\n", 
+          plot_code, 
+          "\n----\nHere is a log of messages from running above code:\n----\n", 
+          log, 
+          "\n----\nPlease try uploading fixed file(s)."))
+      }
       stopifnot(file_exists(plot_png))
-      file_copy(plot_png, plot_www, overwrite = T)
-      
+  
       tabPanel(
         plot_rgn, 
         img(
           src = plot_img,
           alt = glue("Plot output for {dataset$dataset_code}")))
     }
-    
-    message(glue("in output$figs values$plot_success"))
-    values$plot_success <- T
-    
+
     tabset <- do.call(
       tabsetPanel,
       lapply(
         plot_chunks, 
         plot_chunk_to_tab_panel))
-        
-      # },
-      # error = function(e) {
-      #   message(c("ERROR in output$figs(tryCatch())", safeError(e)))
-      #   stop(safeError(e))
-      # })
     
+    message(glue("in output$figs values$plot_success"))
+    values$plot_success <- T    
+    tabset
   })
   
   # output$tbl ----
   output$tbl <- renderDT({
     req(file_input())
     req(values$load_success)
+    req(values$plot_success)
     
     dataset <- get_dataset()
     
@@ -574,11 +771,15 @@ server <- function(input, output, session) {
     tagList(
       "Success loading and plotting data!", br(),
       "Finally, are you ready to", strong("Submit"), "these updated data files for internal review?", br(), br(),
+      
+      textAreaInput("txtComments", "Comments", placeholder = "(optional with submission)"),
+      
       actionButton("btnSubmit", "Submit", class="btn-primary"))
   })
   
+  # observe btnSubmit
   observeEvent(input$btnSubmit, {
-    pr <- git_commit_push_request()
+    pr <- git_commit_push_request(comments = input$txtComments)
     values$submit_success <- T
   })
   
